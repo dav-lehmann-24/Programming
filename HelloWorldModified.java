adaa08fa0b0897de6ed55c0e6711c92eabe33a22
dav-lehmann-24 <191463061+dav-lehmann-24@users.noreply.github.com>
2024-12-17 18:30:15 +0100

    Initial Commit

---
+++ HelloWorldModified.java
@@ -0,0 +1,11 @@
+public class HelloWorldModified {
+
+    public static void main(String[] args) {
+
+        String name = "David";
+        int age = 19;
+
+        System.out.println("Hello everyone, my name is" + name + " and I'm " + age + " years old. Nice to meet you!");
+
+    }
+}
